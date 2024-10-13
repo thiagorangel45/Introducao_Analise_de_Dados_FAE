525b9c4a1eb3908a59e2dcd029d49026371d7925
thiago rangel <thiagorangel@MacBook-Pro-de-thiago.local>
2024-10-12 23:22:42 -0300

    Adicionando exericio 3 da aula 4 RooFit

--- listas de exercicios/macro dos exercicios aula 4/exercicio_3.C
+++ listas de exercicios/macro dos exercicios aula 4/exercicio_3.C
@@ -1,65 +1,49 @@
 #include <iostream>
 #include "TFile.h"
+#include "TH1.h"
 #include "TCanvas.h"
-#include "TLegend.h"
 #include "RooRealVar.h"
-#include "RooCrystalBall.h"
-#include "RooExponential.h"
-#include "RooAddPdf.h"
 #include "RooDataSet.h"
+#include "RooAddPdf.h"
+#include "RooCBShape.h"
+#include "RooPolynomial.h"
 #include "RooPlot.h"
-#include "RooFit.h"
+#include "RooFitResult.h"
+#include "RooFit.h" 
 
 void exercicio_3() {
 
+    TFile *file = TFile::Open("DataSet_lowstat.root");
+    RooDataSet *data = (RooDataSet*)file->Get("data");
 
-    TCanvas *c1 = new TCanvas("c1", "Ajuste JPsi", 800, 600);
-    TFile *file = new TFile("/Users/thiagorangel/UERJ/Introducao_Analise_de_Dados_FAE/data/DataSet_lowstat.root");
-    TTree *tree = (TTree*)file->Get("data");
+    RooRealVar mass("mass", "Massa [GeV/c^{2}]", 2.5, 3.5);  
 
-    // Definindo a variável da massa
-    RooRealVar x("mass", "Massa (GeV/c^{2})", 2.5, 3.5);
-    RooDataSet dataUnbinned("dataUnbinned", "Unbinned data", RooArgSet(x), RooFit::Import(*tree));
-    std::cout << "Dados importados: " << std::endl;
-    dataUnbinned.Print("v");
+    // Definindo a Crystal Ball
+    RooRealVar mean("mean", "Média", 3.096916, 3.0, 3.2); 
+    RooRealVar sigma("sigma", "Desvio Padrão", 0.01, 0.001, 0.1);
+    RooRealVar alpha("alpha", "Alpha", 1.0, 0.1, 5.0);
+    RooRealVar n("n", "n", 1.0, 0.1, 5.0);
+    RooCBShape CB("CB", "Sinal JPsi", mass, mean, sigma, alpha, n);
 
-    // Definindo a função Crystal Ball
-    RooRealVar mean("mean", "Média do sinal", 3.096916, 3.0, 3.2);
-    RooRealVar sigma("sigma", "Largura do sinal", 0.1, 0.01, 0.5);
-    RooRealVar alpha("alpha", "Alpha da Crystal Ball", 1.0, 0, 5);  
-    RooRealVar n("n", "n da Crystal Ball", 2.0, 0, 5);              
-    RooCrystalBall CB("CB", "Sinal JPsi", x, mean, sigma, alpha, n);
+    RooRealVar lambda("lambda", "lambda", 1, 0, 10);
+    RooExponential exp("exp", "background", mass, lambda);
 
-    // Definindo o background exponencial
-    RooRealVar lambda("lambda", "Constante de decaimento", -1.0, -10.0, 0.0); 
-    RooExponential exp("exp", "Background exponencial", x, lambda);
+    RooRealVar nsig("nsig", "Número de Sinal", 200, 0, 1000);
+    RooRealVar nbkg("nbkg", "Número de Fundo", 300, 0, 1000);
+    RooAddPdf model("model", "Modelo Sinal + Fundo", RooArgList(CB, exp), RooArgList(nsig, nbkg));
 
-    // Combinando os modelos de sinal e background
-    RooRealVar frac("frac", "Frações do sinal", 0.5, 0.0, 1.0);
-    RooAddPdf model("model", "Modelo Sinal + Background", RooArgList(CB, exp), RooArgList(frac));
+    RooFitResult *fitResult = model.fitTo(*data);
 
-    // Ajustando o modelo aos dados
-    model.fitTo(dataUnbinned);
+    RooPlot *frame = mass.frame();
+    data->plotOn(frame); 
+    model.plotOn(frame);
+    model.paramOn(frame); 
 
-    // Plotando os resultados
-    RooPlot* frame = x.frame();
-    dataUnbinned.plotOn(frame, RooFit::MarkerColor(kBlue), RooFit::Name("Dados"));
-    model.plotOn(frame, RooFit::LineColor(kRed), RooFit::Name("Ajuste do Modelo"));
-    model.plotOn(frame, RooFit::Components("CB"), RooFit::LineStyle(kDashed), RooFit::LineColor(kGreen), RooFit::Name("Crystal Ball"));
-    model.plotOn(frame, RooFit::Components("exp"), RooFit::LineStyle(kDotted), RooFit::LineColor(kMagenta), RooFit::Name("Exponencial"));
+    double chi2 = frame->chiSquare();
+    std::cout << "chi2 / ndf = " << chi2 << std::endl;
 
-    // Adicionando a legenda
-    TLegend *legend = new TLegend(0.6, 0.7, 0.9, 0.9);  
-    legend->AddEntry(frame->findObject("Dados"), "Dados", "p");
-    legend->AddEntry(frame->findObject("Ajuste do Modelo"), "Modelo (Crystal Ball + Exponencial)", "l");
-    legend->AddEntry(frame->findObject("CB"), "Crystal Ball", "l");
-    legend->AddEntry(frame->findObject("exp"), "Exponencial", "l");
-    legend->SetBorderSize(1); 
-    legend->Draw();
-
-    c1->Update();
-    c1->SaveAs("ImportarDadosFitModel.png");
+    TCanvas *c = new TCanvas("c", "Ajuste JPsi", 800, 600);
+    frame->Draw();
+    c->SaveAs("fit_result.pdf");
     file->Close();
-
-    
 }
