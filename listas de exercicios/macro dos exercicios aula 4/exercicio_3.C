9f9f0ac553e5715f0aed6db6aef920511273d5be
thiago rangel <thiagorangel@MacBook-Pro-de-thiago.local>
2024-10-13 09:56:14 -0300

    Alterando exericio 3 da aula 4 RooFit

--- listas de exercicios/macro dos exercicios aula 4/exercicio_3.C
+++ listas de exercicios/macro dos exercicios aula 4/exercicio_3.C
@@ -16,21 +16,23 @@ void exercicio_3() {
     TFile *file = TFile::Open("DataSet_lowstat.root");
     RooDataSet *data = (RooDataSet*)file->Get("data");
 
-    RooRealVar mass("mass", "Massa [GeV/c^{2}]", 2.5, 3.5);  
+    RooRealVar mass("mass", "Massa [GeV/c^{2}]", 2, 6);  
 
     // Definindo a Crystal Ball
-    RooRealVar mean("mean", "Média", 3.096916, 3.0, 3.2); 
-    RooRealVar sigma("sigma", "Desvio Padrão", 0.01, 0.001, 0.1);
-    RooRealVar alpha("alpha", "Alpha", 1.0, 0.1, 5.0);
-    RooRealVar n("n", "n", 1.0, 0.1, 5.0);
-    RooCBShape CB("CB", "Sinal JPsi", mass, mean, sigma, alpha, n);
+    RooRealVar mean("mean", "mean", 3.1, 2.8, 3.2); 
+    RooRealVar sigma("sigma", "sigma", 0.3, 0.0001, 1.); 
+    RooRealVar alfa("alfa", "alfa", 1.5, -5., 5.); 
+    RooRealVar n("n", "n", 1.5, 0.5, 5.);
+    RooCBShape CB( "CB","CB", mass , mean , sigma , alfa , n);
 
-    RooRealVar lambda("lambda", "lambda", 1, 0, 10);
-    RooExponential exp("exp", "background", mass, lambda);
 
-    RooRealVar nsig("nsig", "Número de Sinal", 200, 0, 1000);
-    RooRealVar nbkg("nbkg", "Número de Fundo", 300, 0, 1000);
-    RooAddPdf model("model", "Modelo Sinal + Fundo", RooArgList(CB, exp), RooArgList(nsig, nbkg));
+    RooRealVar a1("a1", "a1", -0.7, -2., 2.);
+    RooRealVar a2("a2", "a2", 0.3, -2., 2.);
+    RooRealVar a3("a3", "a3", -0.03, -2., 2.);
+    RooPolynomial background("background","The background PDF", mass, RooArgList(a1,a2,a3));
+
+    RooRealVar frac("frac", "frac", 0.5, 0.0, 1.0);
+    RooAddPdf model("model", "Modelo Sinal + Fundo", RooArgList(CB, background), RooArgList(frac));
 
     RooFitResult *fitResult = model.fitTo(*data);
 
